f66cc7de5b2e57d2d543aebdf00fbd4420a60621
João Francisco <joao.bordini42@gmail.com>
2026-03-06 14:40:49 -0300

    Update GerenciadorDeCiclos.java

--- src/Ferramentas/GerenciadorDeCiclos.java
+++ src/Ferramentas/GerenciadorDeCiclos.java
@@ -1,13 +1,33 @@
 package Ferramentas;
 
+import Enums.Biomas;
+import Enums.Tempo;
+import Mundo.MacroChunk;
 import Mundo.Mundo;
 import Simulação.ClimaDiario;
 
 public class GerenciadorDeCiclos {
 
-    public static void atualizarUmCiclo(Mundo mundo){
-        long curCiclo = mundo.getCicloMundial();
-        mundo.setCicloMundial(curCiclo + 1);
+    public static void simularTickMundo(Mundo mundo) {
+        long cicloAtual = mundo.getCicloMundial();
+        
+        mundo.setCicloMundial(cicloAtual + 1);
+        if (cicloAtual == 0) return;
+
+        eventoDoMinuto(mundo);
+        if (cicloAtual % Tempo.HORA.getQntCiclos() == 0L) {
+            eventosDaHora(mundo);
+        }
+
+        if (cicloAtual % Tempo.DIA.getQntCiclos() == 0L) {
+            eventosDoDia(mundo);
+        }
+
+        if (cicloAtual % Tempo.MES.getQntCiclos() == 0L) {
+            eventosDoMes(mundo);
+        }
+    }
+
     private static void eventoDoMinuto(Mundo mundo){
     }
 
